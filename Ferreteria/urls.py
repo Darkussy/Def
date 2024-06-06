4872b9a75661a69bb3596cf6a119eaf49fb916b7
Darkussy <127253787+Darkussy@users.noreply.github.com>
2024-06-06 13:55:04 -0400

    Update urls.py

--- Ferreteria/urls.py
+++ Ferreteria/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import Index, Productos, Prod_1 , Admin,Registrar,delete_Producto, Modificar
 
 urlpatterns = [
-    path("", Index, name="Index"),
+    path('', Index, name="Index"),
     path('Productos/',Productos,name='Productos'),
     path('Prod_1/',Prod_1,name='Prod_1'),
     path('Admin/',Admin,name='Admin'),
